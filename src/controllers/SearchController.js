4e98bb5dedacce629724d4ffcb2e962942089aa6
Andréia Pasqual <pasqual1982.ap@gmail.com>
2020-06-09 14:02:20 -0300

    add

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,10 +1,30 @@
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray')
 
 module.exports = {
     async index(request, response){
-        console.log(request.query);
+        const {latitude, longitude, techs } = request.query;
+
+        const teachsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs: {  
+                $in: teachsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+            } ,
+        });
+
+        return response.json({ devs });
         // busca todos os devs num raio de 10Km
         // filtrar por tecnologias
-        return response.json({ devs: [] });
+        
     }
 }
