c46d3a76bc2c47c67754b34cd10006935351b49f
Andréia Pasqual <pasqual1982.ap@gmail.com>
2020-07-20 14:34:15 -0300

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = requeire('http');
 const routes = require('./routes');
+const {setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://dbAndreia:305306@cluster0-bqn0a.mongodb.net/dbsemana10?retryWrites=true&w=majority',{
 useNewUrlParser: true,
@@ -14,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
 
 
 
